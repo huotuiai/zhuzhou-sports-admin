bbe7c73c03a88ab2c878e141feb1998f4118c965
huotuiai <316448517+huotuiai@users.noreply.github.com>
2026-08-30 10:03:51 +0800

    feat: 添加后台菜单路径配置和说明文档

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/yhsql/admin/',
   plugins: [vue(), tailwindcss()],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
