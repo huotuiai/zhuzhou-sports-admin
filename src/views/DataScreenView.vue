17e8cf047b680df25eff1c1e6224350d19154404
huotuiai <316448517+huotuiai@users.noreply.github.com>
2026-09-06 09:58:38 +0800

    feat: 更新数据大屏的 API URL，切换到生产环境

--- src/views/DataScreenView.vue
+++ src/views/DataScreenView.vue
@@ -15,8 +15,8 @@ import {
 } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
 
-// const DATA_SCREEN_URL = 'https://apizzjj.hnhcsz.com/yhsql/ddp/'
-const DATA_SCREEN_URL = 'http://127.0.0.1:5176/'
+const DATA_SCREEN_URL = 'https://apizzjj.hnhcsz.com/yhsql/ddp/'
+// const DATA_SCREEN_URL = 'http://127.0.0.1:5176/'
 
 const screenRef = ref<HTMLElement | null>(null)
 const promptOpen = ref(true)
